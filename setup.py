c694f23c605ebacdc3c20bf1eab888d430ce3382
Joseph Hall <perlhoser@gmail.com>
2018-02-01 19:35:42 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='Grabbr',
+    version='0.6.0',
+    description='Data mining tool',
+    author='Joseph Hall',
+    author_email='techhat@gmail.com',
+    url='https://github.com/techhat/grabbr',
+    packages=['grabbr'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'beautifulsoup4',
+        'psycopg2',
+        'pyyaml',
+        'requests',
+        'termcolor',
+    ],
+    scripts=['scripts/grabbr'],
+    data_files=[
+        ('share/grabbr', ['schema/grabbr.sql']),
+    ],
+)
