f954388ce67e3d3578826398cd32a4cfe31b71a3
Joseph Hall <perlhoser@gmail.com>
2018-02-18 10:51:18 -0700

    Add start options to salt execution module

--- salt/_modules/grabbr.py
+++ salt/_modules/grabbr.py
@@ -3,6 +3,7 @@
 Salt execution module for Grabbr
 '''
 
+import os
 import json
 
 from salt.ext import six
@@ -42,11 +43,39 @@ def queue(urls, force=False, data=None):
     _query(urls=urls, force=force, data=data)
 
 
-def start():
+def start(
+        config_file='/etc/grabbr/grabbr',
+        run_dir='/var/run/grabbr',
+        module_dir=None,
+    ):
     '''
     Start the Grabbr daemon
     '''
-    __salt__['cmd.run_bg'](('grabbr', '--daemon'))
+    if not os.path.exists(config_file):
+        raise Exception('Config file ({}) not found'.format(config_file))
+
+    if not os.path.exists(os.path.dirname(pid_file)):
+        raise Exception('PID dir ({}) not found'.format(os.path.dirname(pid_file)))
+
+    args = (
+        'grabbr', '--daemon',
+        '--config-file', config_file,
+        '--run-dir', run_dir,
+        '--module-dir', module_dir,
+    )
+
+    if module_dir is not None:
+        if isinstance(module_dir, str):
+            module_dir = [module_dir]
+        if not isinstance(module_dir, list):
+            raise Exception('module_dir must be a string or list')
+        for item in module_dir:
+            if not os.path.exists(item):
+                raise Exception('module_dir {} does not exist')
+        args.append('--module-dir')
+        args.extend(module_dir)
+
+    __salt__['cmd.run_bg'](args)
 
 
 def stop():
