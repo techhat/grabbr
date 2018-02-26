bf731bc795298d4d586aebb80ba4c7bd1880f2b4
Joseph Hall <perlhoser@gmail.com>
2018-02-26 06:53:20 -0700

    Don't fail if there's no meta file

--- salt/_grains/grabbr.py
+++ salt/_grains/grabbr.py
@@ -25,6 +25,8 @@ def process():
     run_dir = __opts__.get('grabbr_run_dir', '/var/run/grabbr')
     for agent in os.listdir(run_dir):
         meta_file = os.path.join(run_dir, agent, 'meta')
+        if not os.path.exists(meta_file):
+            continue
         with open(meta_file, 'r') as mfh:
             meta = json.load(mfh)
             if psutil.Process(meta['pid']).cmdline()[0]:
