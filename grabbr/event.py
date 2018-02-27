ef2311a7755e896db4829c6904964f708337418c
Joseph Hall <perlhoser@gmail.com>
2018-02-27 06:55:56 -0700

    Fire minion events correctly

--- grabbr/event.py
+++ grabbr/event.py
@@ -13,12 +13,13 @@ def bus(opts):
     '''
     Connect to Salt's event bus
     '''
-    salt_opts = salt.config.master_config('/etc/salt/{}'.format(opts['salt_node']))
+    salt_opts = salt.config.minion_config('/etc/salt/{}'.format(opts['salt_node']))
 
     event = salt.utils.event.get_event(
         opts['salt_node'],
         salt_opts['sock_dir'],
         salt_opts['transport'],
+        opts=salt_opts,
         listen=False,
     )
 
@@ -32,4 +33,4 @@ def fire(tag, data, opts):
     Fire a message on the event bus
     '''
     if opts['salt_events'] is True:
-        opts['salt_event'].fire_event(data, tag)
+        opts['salt_event'].fire_master(data, tag)
